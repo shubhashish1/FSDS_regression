6273734473162bc582a30c48e3baf984177d007f
shubhashish1 <shubhashish.mishra20@gmail.com>
2023-09-02 20:51:03 +0530

    sixth commit

--- src/exception.py
+++ src/exception.py
@@ -5,19 +5,34 @@ Here we will be writing the custom exception
 import sys
 from src.logger import logging
 
-def error_message_detail(error, error_detail : sys):
-    _,_,exc_tb = error_detail.exc_info() # Exc_info returns 3 arguments
+
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
 
-    error_message = f"Error occured in python script name [{file_name}] line_number [{exc_tb.lineno}] and" \
-                    f"error mesaage [{error}] "
+    error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
+    )
 
     return error_message
 
-class customException(Exception): # Here it inherits from Exception super class
-    def __init__(self,error_message, error_detail: sys):
-        super().__init__(error_message) # Here we are importing the init of the Exception class
-        self.error_message = error_message_detail(error_message = error_message,error_details = error_detail)
+
+class CustomException(Exception):
+
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
+
+
+if __name__ == "__main__":
+    logging.info("logging started")
+
+    try:
+        1/0
+        logging.info('diving data')
+    except Exception as e:
+        logging.error(e)
+        raise CustomException(e,sys)
